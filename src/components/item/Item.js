8106c7299ed9a12713f8ed5b5e4122bc63085c34
kavitasuthar123 <kavitasuthar.in@gmail.com>
2024-07-18 10:14:28 +0530

    first commit

---
+++ src/components/item/Item.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Item.css";
+
+
+
+
+<img src="path/to/image" alt="Detailed description of the item" />
+
+
+
+
+
+
+
+function Item({ name, rating, price, saleDiscount, image, brand }) {
+  return (
+    <div className="item-card">
+      <img src={image} alt={"Item image"} width="100%" />
+      <div className="item-brand">{brand}</div>
+      <div className="item-name">{name}</div>
+      <div className="item-info">
+        <div className="item-price">${price}</div>
+        <div className="item-rating">{rating}&#9733;</div>
+      </div>
+    </div>
+  );
+}
+
+export default Item;
